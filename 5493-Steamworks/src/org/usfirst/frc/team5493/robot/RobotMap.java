90684657a4da4f442da18b7b5eda9f8b41e07d78
msliv <msliv@192.168.2.40>
2017-02-11 10:21:45 -0700

    Comments - and suggestions.

--- 5493-Steamworks/src/org/usfirst/frc/team5493/robot/RobotMap.java
+++ 5493-Steamworks/src/org/usfirst/frc/team5493/robot/RobotMap.java
@@ -19,18 +19,22 @@ public class RobotMap {
 	public final static int JOYBTN_TRIGGER = 1;
 	
 	//Talon #'s from the web dashboard
+	//BKE -- Are these drive motors or ropeMotors - better comments and names help in a pinch.
     public static int leftFront = 5;
     public static int rightFront = 3;
     public static int leftBack = 1;
     public static int rightBack = 8;
+    //BKE - Rope Motor is two right?
     public static int ropeMotor = 9;
     public static int gearMotor = 2;
 	
+    //BKE - Better names would allow you or anyone new to the code to know what the pneumatics1 and pneumatics2 do?
 	public static int pneumatics1 = 0;
 	public static int pneumatics2 = 1;
 	public static int compressorNum = 0;
 	
 	public static SpeedController SpeedController(int port) {
+		//BKE - You only need this if you have TalonSR (LWTalon) AND TalonSRX (CANTalon) mixes on your bot or a practice bot with TalonSR and a comp bot with TalonSRX.
 		if (Timings.USE_CAN) {
 			return new CANTalon(port);
 		}
